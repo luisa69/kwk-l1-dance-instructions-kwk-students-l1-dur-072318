43a936b81d0a2eef2735f12183e0529a17cdf2e6
Luisa Pichardo <luisapichardo@gmail.com>
2018-07-23 20:33:43 +0000

    automatically backed up by learn

--- dance_instructions.rb
+++ dance_instructions.rb
@@ -6,4 +6,8 @@ def gangnam style
     puts "2: Lift right foot."
     puts "3: Return right foot to the ground."
     puts "4: Finishing with a small skip-step backward."
-    puts 
+    puts "5: Lower left foot to the ground."
+    puts "6: Bounce left foot back up slightly, kicking it a few inches back."
+    puts "7: Left, Right, Left and Left."
+    puts "8: Lower right foot to the ground."
+    puts ""
