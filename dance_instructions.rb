68802658f718267afe0542143ff87fa9ffddcf8a
Luisa Pichardo <luisapichardo@gmail.com>
2018-07-23 20:27:06 +0000

    automatically backed up by learn

--- dance_instructions.rb
+++ dance_instructions.rb
@@ -1,3 +1,9 @@
 
 # Code your methods below
 
+def gangnam style 
+    puts "1: Plant leegs far apart, bend kneees slightly and keep poature loose."
+    puts "2: Lift right foot."
+    puts "3: Return right foot to the ground."
+    puts "4: Finishing with a small skip-step backward."
+    puts 
